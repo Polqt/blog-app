e3440839a04e93060cf4419d3b4a551929ff0835
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-09-19 04:04:48 +0800

    Finish the partial parts

--- index.js
+++ index.js
@@ -8,14 +8,24 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
 
-app.post("/submit", (req, res) => {
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+})
 
-});
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+})
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
