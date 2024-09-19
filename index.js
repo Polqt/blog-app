027322131e4786d82b3806cdddff773017e27774
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-09-20 00:08:55 +0800

    Finish the Contact and About page.

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", );
 });
-
 
 app.get("/about", (req, res) => {
     res.render("about.ejs");
@@ -26,6 +25,20 @@ app.get("/create", (req, res) => {
     res.render("create.ejs");
 })
 
+app.post("/contact", (req, res) => {
+    const firstName = req.body.first;
+    const lastName = req.body.last;
+    const email = req.body.email;
+    const message = req.body.message;
+
+    if (!firstName || !lastName || !email || !message) {
+        console.log("Please fill out all fields");
+        res.redirect('/contact?error=Please fill out all fields');
+    } else {
+        console.log(`Contact form submitted by ${firstName} ${lastName} with an email of ${email} and a message of: ${message}`);
+        res.redirect('/contact?success=Form submitted successfully!');
+    };
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
